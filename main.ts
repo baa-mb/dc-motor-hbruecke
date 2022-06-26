9a66bbb504a78e5e4f37b3d92eb0988ad6ee3d21
baa <34665120+baa-mb@users.noreply.github.com>
2022-06-26 18:29:25 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,20 +6,24 @@ input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     lauf = 0
 })
+let distanz_alt = 0
 let lauf = 0
-basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.Yes)
 let wert = 300
 lauf = 0
+let distanz = 0
 basic.forever(function () {
     if (lauf == 1) {
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.analogWritePin(AnalogPin.P0, 850)
-        basic.pause(20000)
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        basic.pause(8000)
-        wert += 200
-        wert = wert % 1024
+        basic.pause(1000)
+        distanz = sonar.ping(
+        DigitalPin.P8,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+        )
+        if (distanz != distanz_alt) {
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            pins.analogWritePin(AnalogPin.P0, Math.map(distanz, 1, 10, 0, 850))
+            distanz_alt = distanz
+        }
     }
-    serial.writeLine("" + (wert))
 })
